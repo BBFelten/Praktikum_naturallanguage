bc86ef6144c3a7ae9e9e26a2da948d892168acae
befe326c <bettina.felten@mailbox.tu-dresden.de>
2024-05-28 19:08:24 +0200

    Fix bugs, add comments

--- pcfg/parse.py
+++ pcfg/parse.py
@@ -1,4 +1,11 @@
+import heapq
+
 def unary_closure(rules, queue):
+    """Function that finds unary rules for each cell
+    Parameters:
+        rules: dictionary of rules in the form (left side of rule, (right side of rule)) -> weight
+        queue: priority queue containing tuples of (negative) weights and non-terminals
+    """
     weights = {}
     result = {}
     for c in queue:
@@ -6,47 +13,72 @@ def unary_closure(rules, queue):
 
     backtraces = {}
     while len(queue) > 0:
-        current = max(queue, key=lambda x: x[1])
-        queue.remove(current)
-        if weights[current[0]] < current[1]:
-            weights[current[0]] = current[1]
+        current = heapq.heappop(queue)
+        current_weight = -current[0]
+        current_nt = current[1]
+        if not current_nt in weights:
+            weights[current_nt] = 0
+        if weights[current_nt] <= current_weight:
+            weights[current_nt] = current_weight
 
             for left, right in rules:
-                if len(right) == 1 and current[0] == right[0]:
-                    w = rules[(left, right)] * current[1]
-                    queue.append((left, w))
-                    weights[left] = w
-                    result[left] = w
-                    backtraces[left] = right[0]
-            
+                right = [right] if isinstance(right, int) else right
+                if len(right) == 1 and current_nt == right[0]:
+                    w = rules[(left, right)] * current_weight
+                    if left not in weights or w > weights[left]:
+                        heapq.heappush(queue, (-w, left))
+                        weights[left] = w
+                        result[left] = w
+                        backtraces[left] = right[0]
+
     return result, backtraces
 
 
-def cyk_parse(sentence, id_dict, lex_left, lex_wgts, N, R, initial="ROOT"):
+def cyk_parse(sentence, id_dict, lex_left, lex_rules, N, R, initial="ROOT"):
+    """Function that implements the cyk-parse algorithm
+    Parameters:
+        sentence: str
+        id_dict: dictionary that maps words to indices
+        lex_left: list of non-terminals that are on the left side of lexicon rules
+        lex_rules: dictionary of lexicon rules of the form (non-terminal, word index) -> weight
+        N: set of non-terminals
+        R: dictionary of grammar rules in the form (left side of rule, (right side of rule)) -> weight
+        initial: str that represents root of tree
+    returns:
+        tbl: dictionary of the form (i, j, non-terminal) -> weight
+        root_id: tuple of indices that contain the root
+        backtraces: dictionary containing backtraces of the form (i, j, non-terminal) -> [right side of rule, m]
+    """
     words = [w.strip() for w in sentence.split(' ')]
     words = [w for w in words if w != '']
     n = len(words)
-    done = False
+    found = False
 
     tbl = {}
     backtraces = {}
+    # iterate over indices and get lexicon rules
     for i in range(1,n+1):
         word_int = id_dict[words[i-1]]
         A = lex_left[word_int]
-        w = lex_wgts[word_int]
+        w = lex_rules[(A, word_int)]
         tbl[(i-1, i, A)] = w
         backtraces[(i-1, i, A)] = [word_int]
-        unary_weights, _ = unary_closure(R, [(A, w)])
+        unary_weights, unary_backtraces = unary_closure({**R, **lex_rules}, [(-w, A)])
         for nonterminal, weight in unary_weights.items():
             if weight > 0:
                 tbl[(i-1, i, nonterminal)] = weight
-                backtraces[(i-1, i, nonterminal)] = [word_int]
-
-    for r in range(2,n):
-        for i in range(n-r):
+                backtraces[(i-1, i, nonterminal)] = [unary_backtraces[nonterminal]]
+    
+    # width of span
+    for r in range(2,n+1):
+        queue = []
+        # left limit
+        for i in range(n+1-r):
+            # right limit
             j = i+r
+            # split of span
             for m in range(i+1, j):
-                queue = []
+                # add binary rules
                 for A in N:
                     for rule in R:
                         if len(rule[1]) == 2 and A == rule[0]:
@@ -55,78 +87,97 @@ def cyk_parse(sentence, id_dict, lex_left, lex_wgts, N, R, initial="ROOT"):
                                 w = R[rule] * tbl[(i,m,B)] * tbl[(m,j,C)]
                                 if (i,j, A) in tbl:
                                     if w > tbl[(i,j,A)]: 
-                                        queue.remove((A, tbl[(i,j,A)]))
-                                        queue.append((A, w))
+                                        if (A, tbl[(i,j,A)]) in queue:
+                                            queue.remove((A, tbl[(i,j,A)]))
+                                        heapq.heappush(queue, (-w, A))
                                         tbl[(i,j,A)] = w
                                         backtraces[(i,j,A)] = [B, C, m]
-                                        # if A == initial and j == n:
-                                        if A == initial:
-                                            root_id = [i,j]
-                                            done = True
+
                                 else:
                                     tbl[(i,j,A)] = w
                                     backtraces[(i,j,A)] = [B, C, m]
-                                    queue.append((A, w))
-                                    # if A == initial and j == n:
-                                    if A == initial:
-                                        root_id = [i,j]
-                                        done = True
-
-                    if done:
-                        # print("FOUND!")
-                        return tbl, root_id, backtraces
+                                    heapq.heappush(queue, (-w, A))
+                    
+                                if A == initial and i == 0 and j == n:
+                                    root_id = [i,j]
+                                    found = True
                 
+                # add unary rules
                 unary_weights, unary_backtraces = unary_closure(R, queue)
                 for nonterminal, weight in unary_weights.items():
                     if weight > 0:
                         tbl[(i,j,nonterminal)] = weight
                         backtraces[(i,j,nonterminal)] = [unary_backtraces[nonterminal]]
-                    # if nonterminal == initial and j == n:
-                    if nonterminal == initial:
+                    # check if the root was reached
+                    if nonterminal == initial and i == 0 and j == n:
                         root_id = [i,j]
-                        done = True
+                        found = True
     
-    # print("NOT FOUND!")
+    if found:
+        return tbl, root_id, backtraces
+    
     return False, (), {}
 
 
 def best_tree(backtraces, root, word_dict):
+    """Get the best solution from the backtraces
+    Parameters:
+        backtraces: dictionary containing backtraces of the form (i, j, non-terminal) -> [right side of rule, m]
+        root: str that marks the root of the tree
+        word_dict: dictionary that maps indices to words
+    """
     if isinstance(root[3], int):
         return (root[2], word_dict[root[3]])
     elif len(root) < 6:
         i, j, A, B = root[:4]
         next = tuple([i, j, B] + backtraces[(i, j, B)])
-        return (A, B), best_tree(backtraces, next, word_dict)
+        return (A, best_tree(backtraces, next, word_dict))
     else:
         i, j, A, B, C, m = root
         left = tuple([i, m, B] + backtraces[(i, m, B)])
         right = tuple([m, j, C] + backtraces[(m, j, C)])
-        return (A, B, C), best_tree(backtraces, left, word_dict), best_tree(backtraces, right, word_dict)
+        return (A, best_tree(backtraces, left, word_dict), best_tree(backtraces, right, word_dict))
 
 
 def get_lexicon(lexicon):
+    """Get non-terminal rules from lexicon file
+    Parameters:
+        lexicon: path to lexicon file
+    returns:
+        lex_left: list of non-terminals that point directly to words
+        lex_rules: dictionary of lexicon rules of the form (non-terminal, word index) -> weight
+        N: Set of non-terminals
+        id_dict: dictionary that maps words to indices
+        word_dict: dictionary that maps indices to words
+    """
     words = []
+    lex_rules = {}
     lex_left = []
-    lex_wgts = []
-    N = set()
-    with open(lexicon, 'r') as lex_file:
-        for l in lex_file:
-            l_splt = [elem.strip() for elem in l.split(' ')]
-            words.append(l_splt[1])
-            lex_left.append(l_splt[0])
-            lex_wgts.append(float(l_splt[2]))
-            N.add(l_splt[0])
-    
     id_dict = {}
     word_dict = {}
-    for i, w in enumerate(words):
-        id_dict[w] = i
-        word_dict[i] = w
+    N = set()
+    with open(lexicon, 'r') as lex_file:
+        for i,l in enumerate(lex_file):
+            l_splt = [elem.strip() for elem in l.split(' ')]
+            words.append(l_splt[1])
+            id_dict[l_splt[1]] = i
+            word_dict[i] = l_splt[1]
+            lex_rules[(l_splt[0], (i))] = float(l_splt[2])
+            lex_left.append(l_splt[0])
+            N.add(l_splt[0])
     
-    return lex_left, lex_wgts, N, id_dict, word_dict
+    return lex_left, lex_rules, N, id_dict, word_dict
 
 
 def get_rules(rules, N):
+    """Get rules from grammar file
+    Parameters:
+        rules: path to grammar file
+        N: list of non-terminals
+    returns:
+        rls: dictionary of (left side of rule, (right side of rule)) -> weight
+        N: completed set of non-terminals
+    """
     rls = {}
     with open(rules, 'r') as rls_file:
         for r in rls_file:
@@ -143,30 +194,40 @@ def get_rules(rules, N):
 
 
 def run_cyk_parse(rules, lexicon, sentences, initial="ROOT"):
-    lex_left, lex_wgts, N, id_dict, word_dict = get_lexicon(lexicon)
+    """Run the cyk parse algorithm
+    Parameters:
+        rules: path to file containing grammar rules
+        lexicon: path to file containing lexicon
+        sentences: list of strings
+        initial(str): root symbol of the parse tree
+    """
+    lex_left, lex_rules, N, id_dict, word_dict = get_lexicon(lexicon)
     rls, N = get_rules(rules, N)
-
     trees = []
     for sentence in sentences:
         sentence = sentence.strip()
-        result, root_id, backtraces = cyk_parse(sentence, id_dict, lex_left, lex_wgts, N, rls, initial)
+        result, root_id, backtraces = cyk_parse(sentence, id_dict, lex_left, lex_rules, N, rls, initial)
         
         if result:
             root_bt = backtraces[tuple(root_id + [initial])]
             root = tuple(root_id + [initial] + root_bt)
             tree = best_tree(backtraces, root, word_dict)
-            # print(tree)
             trees.append(tree)
         
         else:
-            trees.append(('NOPARSE ' + sentence))
+            trees.append('(NOPARSE {})'.format(sentence))
     
     for t in trees:
-        print(t)
-    return trees
+        print(str(t).replace("'", "").replace(",", ""))
+    # return trees
 
 
 if __name__ == "__main__":
-    # sentence = "The proposed holding company 's primary purpose would be to allow Great American to continue engaging in real estate development activities , it said ."
-    sentence = "A Lorillard spokewoman said , `` This is an old story ."
-    run_cyk_parse("./material/small/grammar.rules", "./material/small/grammar.lexicon", [sentence], initial="ROOT")
+    # sentence = "A Lorillard spokewoman said , `` This is an old story ."
+    # run_cyk_parse("./material/small/grammar.rules", "./material/small/grammar.lexicon", [sentence], initial="ROOT")
+
+    # sentence = "The new real estate unit would have a separate capital structure to comply with the law ."
+    # run_cyk_parse("./material/large/grammar.rules", "./material/large/grammar.lexicon", [sentence], initial="ROOT")
+
+    sentences = ["a b", "a a b b b", "b a", "a a", "b" ]
+    run_cyk_parse("./tests/data/test.rules", "./tests/data/test.lexicon", sentences)
